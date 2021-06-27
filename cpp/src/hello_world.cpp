b51148b9d22cdeb7643df5a2570dadce8a6c9e74
yohuanyang <yohuanyang@synology.com>
2021-06-27 22:01:07 +0800

    greet someone

--- cpp/src/hello_world.cpp
+++ cpp/src/hello_world.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <string>
 
+std::string get_greet(const std::string &who)
+{
+  return "Hello " + who;
+}
+
 int main(int argc, char **argv)
 {
-  std::cout << "Hello world" << std::endl;
+  std::string who = "world";
+  if (argc > 1)
+  {
+    who = argv[1];
+  }
+
+  std::cout << get_greet(who) << std::endl;
   return 0;
 }

06ff0ffb068d8ec49f2be74513d17bb68253d2f9
yohuanyang <yohuanyang@synology.com>
2021-06-27 22:25:05 +0800

    add greeting

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,2 +1,10 @@
+import sys
+
+
+def get_greet(who: str) -> str:
+    return f"Hello {who}"
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    who = sys.argv[1] if len(sys.argv) > 1 else "world"
+    print(get_greet(who))

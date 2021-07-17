9fa85bece2f9ca8dcb548ff902bd9cd6a3ad35fd
yohuanyang <yohuanyang@synology.com>
2021-07-17 12:19:02 +0800

    add greeting server

---
+++ python/greeting_server.py
@@ -0,0 +1,27 @@
+from concurrent import futures
+import logging
+
+import grpc
+
+from python.lib.proto import greeting_pb2
+from python.lib.proto import greeting_pb2_grpc
+
+_NUM_WORKERS = 10
+
+
+class Greeter(greeting_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return greeting_pb2.HelloResp(message=f"Hello, {request.name}")
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=_NUM_WORKERS))
+    greeting_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()

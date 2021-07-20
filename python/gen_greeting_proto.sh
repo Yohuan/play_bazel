00657c0204936e9f69692f39fd7580a7e3a284a5
yohuanyang <yohuanyang@synology.com>
2021-07-20 15:27:18 +0800

    delete generated python stubs & script

--- python/gen_greeting_proto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir ./lib/proto
-python -m grpc_tools.protoc \
-    -I../protos\
-    --python_out=./lib/proto\
-    --grpc_python_out=./lib/proto\
-    ../protos/greeting.proto
